00f47814be084954a41d08593c8b5f54e05fa598
El-PongO <jc.kho2007@gmail.com>
2026-07-08 23:24:54 +0700

    made user profile

--- back_end/routes/users.js
+++ back_end/routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { verifyJWT } = require("../middlewares/verifyJWT");
 
 // Import controller yang baru dibuat
 const userController = require("../controllers/userController");
@@ -9,5 +10,7 @@ router.post("/api/user/register", userController.register);
 router.post("/api/user/login", userController.login);
 router.get("/api/user/refresh", userController.refresh);
 router.post("/api/user/logout", userController.logout);
+router.put("/api/user/profile", verifyJWT, userController.updateProfile);
+router.delete("/api/user/account", verifyJWT, userController.deleteAccount);
 
 module.exports = router;
